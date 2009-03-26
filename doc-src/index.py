63fe90f6913e6204a92f620257a3d26194f409e5
Samiuela LV Taufa <sam@nullcube.com>
2009-03-26 17:49:09 +1100

    Basic Document updates for countershape

--- doc-src/index.py
+++ doc-src/index.py
@@ -1,15 +1,20 @@
-import countershape.widgets
 import countershape.layout
+import countershape.template
+import countershape.widgets
 import countershape.grok
 from countershape.doc import *
+
+this.layout = countershape.layout.Layout("_layout.html")
+this.markdown = "rst"
 
 ns.docTitle = "Tinytree Manual"
 ns.docMaintainer = "Aldo Cortesi"
 ns.docMaintainerEmail = "aldo@nullcube.com"
 ns.foot = "Copyright Nullcube 2008"
 ns.head = readFrom("_header.html")
-ns.sidebar = countershape.widgets.SiblingPageIndex('/index.html', exclude=['countershape'])
-this.layout = countershape.layout.TwoPane("yui-t2", "doc3")
+ns.sidebar = countershape.widgets.SiblingPageIndex(
+            '/index.html', exclude=['countershape']
+          )
 this.titlePrefix = "TinyTree Manual - "
 
 ns.ctgrok = countershape.grok.grok("../tinytree.py")
