92c36058dbb54d49cc76e6be355382b61689c485
Samiuela LV Taufa <sam@nullcube.com>
2009-03-27 09:00:04 +1100

    One loose line not updated correctly "ns.head = ..."

--- doc-src/index.py
+++ doc-src/index.py
@@ -11,7 +11,7 @@ ns.docTitle = "Tinytree Manual"
 ns.docMaintainer = "Aldo Cortesi"
 ns.docMaintainerEmail = "aldo@nullcube.com"
 ns.foot = "Copyright Nullcube 2008"
-ns.head = readFrom("_header.html")
+ns.head = countershape.template.File(None, "_header.html")
 ns.sidebar = countershape.widgets.SiblingPageIndex(
             '/index.html', exclude=['countershape']
           )
